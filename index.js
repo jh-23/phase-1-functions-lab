e068d14b680849bd51e1abd441a382e9f93ff82e
John Heger <heger7@gmail.com>
2023-10-11 11:39:41 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(start) {
+    const scuberHQ = 42;
+    return(Math.abs(start - scuberHQ))
+}
+
+function distanceFromHqInFeet(start) {
+    distanceFromHqInBlocks(start);
+    const scuberHQ = 42;
+    return(Math.abs(start - scuberHQ)*264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return(Math.abs(start - destination)*264)
+}
+
+function calculatesFarePrice(start, destination) {
+    if (Math.abs(start - destination)*264 <= 400) {
+        return 0
+    } else if (Math.abs(start - destination)*264 > 400 && (Math.abs(start - destination)*264 < 2000)) {
+        return(0.02 * ((Math.abs(start - destination)*264) - 400))
+    } else if (Math.abs(start - destination)* 264 > 2000 && (Math.abs(start - destination)*264 < 2500)) {
+        return 25
+    } else (Math.abs(start - destination)*264 > 2500); {
+        return 'cannot travel that far'
+    }   
+}
